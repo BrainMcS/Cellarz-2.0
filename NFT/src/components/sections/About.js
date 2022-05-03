9b0725e05ae9a3ba814fc39b4a19ceb0cb37c184
BrainMcS <lakobms@gmail.com>
2022-05-04 00:13:26 +0200

    update the dec to cellarz visions and purpose

--- NFT/src/components/sections/About.js
+++ NFT/src/components/sections/About.js
@@ -73,13 +73,13 @@ const About = () => {
         <Box> <Carousel /> </Box>
         <Box>
           <Title>           
-            Welcome To The Weirdos Club.
+            Welcome To Cellarz.
           </Title>
           <SubText>
-            The WEIRDOS CLUB is a private collection of NFTs—unique digital collectibles. The Weirdos are stored as ERC-721 tokens on the Ethereum blockchain and hosted on IPFS.
+            Cellarz is a private collection of VOD-NFTs as unique digital collectibles. The Cellarz assets are stored as ERC-721 tokens on the Ethereum blockchain and hosted on IPFS.
           </SubText>
           <SubTextLight>
-            With more than 200+ hand drawn traits, each NFT is unique and comes with a membership to an exclusive group of successful investors. Join an ambitious ever-growing community with multiple benefits and utilities.
+            More than just an exclusive VOD-NFT platform, each NFT is unique and comes with a membership to an exclusive group of successful investors and shows. Join an ambitious ever-growing community with multiple benefits and utilities, and support our content craetors to help them produce your favorite shows!.
           </SubTextLight>
           <ButtonContainer>
           <ThemeProvider theme={dark}>
